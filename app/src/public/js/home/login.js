ffa2cd2ebbda86da4ff67ff62139fb74b3a359eb
aydenote <dslgpgh@gmail.com>
2023-05-30 22:22:28 +0900

    DOM을 이용한 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('1');
+const id = document.querySelector('#id'),
+  password = document.querySelector('#pw'),
+  loginBtn = document.querySelector('button');
+
+const clickLogin = () => {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+};
+
+loginBtn.addEventListener('click', clickLogin);
