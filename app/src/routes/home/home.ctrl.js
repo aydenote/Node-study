88c05b740cb802877bf2bd26eaa04dc538f5b21c
aydenote <dslgpgh@gmail.com>
2023-05-31 18:08:51 +0900

    로그인 응답 관련 변수 설정

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -18,16 +18,17 @@ const process = {
   login: (req, res) => {
     const id = req.body.id;
     const password = req.body.password;
+    const response = {};
     if (users.id.includes(id)) {
       const idx = users.id.indexOf(id);
       if (users.password[idx] === password) {
-        return res.json({ success: true });
+        response.success = true;
+        return res.json(response);
       }
     }
-    return res.json({
-      success: false,
-      msg: '로그인에 실패하셨습니다.',
-    });
+    response.success = false;
+    response.msg = '로그인에 실패하셨습니다.';
+    return res.json(response);
   },
 };
 
