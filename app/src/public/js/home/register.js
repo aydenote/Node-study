879ddac2ff67bae15a945f4ab4146b9c44283644
aydenote <dslgpgh@gmail.com>
2023-06-01 22:19:42 +0900

    fs 모듈을 통한 회원가입 기능 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -8,7 +8,7 @@ const id = document.querySelector('#id'),
 
 const clickregister = () => {
   if (!id.value) return alert('아이디를 입력해주세요.');
-  if (password !== confirmPw) return alert('비밀번호가 일치하지 않습니다.');
+  if (password.value !== confirmPw.value) return alert('비밀번호가 일치하지 않습니다.');
 
   const req = {
     id: id.value,
