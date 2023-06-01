f31a6bb9fbe670fd94c745c557c087365e5c1878
aydenote <dslgpgh@gmail.com>
2023-06-01 19:02:56 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,14 +1,19 @@
 'use strict';
 
+const fs = require('fs').promises;
 class UserStorage {
-  static #users = {
-    id: ['test1', 'test2', 'test3'],
-    password: ['1234', '12345', '123456'],
-    name: ['최부장', 'ayden', '나천재'],
-  };
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -19,18 +24,15 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    return fs
+      .readFile('./src/databases/users.json')
+      .then(data => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error());
   }
 
   static save(userInfo) {
-    const user = this.#users;
     user.id.push(userInfo.id);
     user.name.push(userInfo.name);
     user.password.push(userInfo.password);
