1391faf272ef0a8f6bcb79c6fb0ce1fa6b646116
aydenote <dslgpgh@gmail.com>
2023-06-04 17:18:01 +0900

    winston 콘솔로 로그 출력 구현

--- app/app.js
+++ app/app.js
@@ -23,14 +23,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
-const accessLogStream = require('./src/config/log');
 
 const app = express();
 dotenv.config();
 
 // 라우팅
 const home = require('./src/routes/home');
+
+const logger = require('./src/config/logger');
+logger.error('안녕하세요');
 
 // 앱 세팅
 app.set('views', './src/views');
@@ -39,8 +40,6 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
-app.use(morgan('common', { stream: accessLogStream }));
 
 app.use('/', home); // use -> 미들 웨어 등록해주는 메서드.
 
